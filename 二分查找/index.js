d60cbc7aeadb54a4e11d8f57a861ff0a36d409d3
张亚超 <zhangyachao@xiaoheiban.cn>
2020-11-25 11:45:14 +0800

    多维数组数组扁平化解提题

--- 二分查找/index.js
+++ 二分查找/index.js
@@ -1,1 +1,15 @@
-console.log("?????")
+//  有序矩阵中第K小的元素
+var matrix = [
+  [ 1,  5,  9],
+  [10, 11, 13],
+  [12, 13, 15]
+]
+var k = 8
+
+// 返回 13。
+
+var kthSmallest = (arr, index) => {
+  return arr.flat().sort((a, b) => a - b)[index-1];
+}
+
+// console.log(kthSmallest(matrix, k));
